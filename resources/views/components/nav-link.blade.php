5d2e5d077c8fedbd504b3b64c09ecd50b8891181
Keys <keys14052002@gmail.com>
2025-08-19 18:21:25 +0000

    homework(props - day 5): introduce a `type` prop to indicate an anchor tag nav-link or button tag nav-link

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,8 +1,19 @@
-@props(["active" => false])
-<a 
-    aria-current="page" 
-    class="{{ $active ? "rounded-md bg-gray-950/50 px-3 py-2 text-sm font-medium text-white" : "rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white" }}"
-    {{ $attributes }}
->
-    {{ $slot }}
-</a>
+@props(["active" => false, "type" => "a"])
+@if($type == "a") 
+    <a 
+        aria-current="page" 
+        class="{{ $active ? "rounded-md bg-gray-950/50 px-3 py-2 text-sm font-medium text-white" : "rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white" }}"
+        {{ $attributes }}
+    >
+        {{ $slot }}
+    </a>
+@else 
+    <button 
+        aria-current="page" 
+        class="{{ $active ? "rounded-md bg-gray-950/50 px-3 py-2 text-sm font-medium text-white" : "rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white" }}"
+        {{ $attributes }}
+    >
+        {{ $slot }}
+    </button>
+
+@endif
