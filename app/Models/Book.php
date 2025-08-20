c6548081b1bf4a19d05a6e0af7fb434e9fd80898
Keys <keys14052002@gmail.com>
2025-08-20 08:20:46 +0000

    feat: setup sqlite database and load book records

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,30 +2,9 @@
 namespace App\Models;
 
 use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Book
+class Book extends Model
 {
-    public static function all(): array {
-        return [
-                [
-                    "id" => 1,
-                    "title" => "Deep Work",
-                    "author" => "Cal Newport"
-                ],
-                [
-                    "id" => 2,
-                    "title" => "Meditations",
-                    "author" => "Marcus Aurelius"
-                ],
-                [
-                    "id" => 3,
-                    "title" => "Limitless",
-                    "author" => "Jim Kwik"
-                ],
-        ];
-    }
-
-    public static function find(int $id): array {
-        return Arr::first(self::all(), fn($book) => $id == $book['id']);
-    }
+    protected $table = "book_listings";
 }
