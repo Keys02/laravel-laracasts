69c0872bd06527497fdda157fd83e2c78daa1550
Keys <keys14052002@gmail.com>
2025-08-20 12:45:13 +0000

    style: change the title of the book listing page

--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -1,7 +1,7 @@
 <x-skeleton>
-        <x-slot:heading>
-            Job Listings
-        </x-slot:heading>
-        <h1 class="font-bold text-lg pl-3">{{ $book['title'] }}</h1>
-        <p class="pl-3">This book was written by {{$book['author']}}.</p>
+    <x-slot:heading>
+        Book Listing
+    </x-slot:heading>
+    <h1 class="font-bold text-lg pl-3">{{ $book['title'] }}</h1>
+    <p class="pl-3">This book was written by {{$book['author']}}.</p>
 </x-skeleton>
